c4c36f189561b5b91003b8dbade80e7712095ea7
dumi97 <giboz97@interia.pl>
2020-03-22 17:06:58 +0100

    Add project files.

---
+++ GIS-Graph-Transformation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIS_Graph_Transformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
